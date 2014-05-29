d3f5eb5f4f365c0e78e437f064f79d572b05254d
Joseph Scheuhammer <clown@alum.mit.edu>
2014-05-29 15:31:31 -0400

    Added link to section "Children Presentational:  True" in the spec.
    - Moved data structure out of 'resolveReferences.js' to respecConfig.ariaSpecURLs.
    - Fixed some links to the spec document, including some named anchors.

--- implementation/js/resolveReferences.js
+++ implementation/js/resolveReferences.js
@@ -1,14 +1,8 @@
 function linkCrossReferences() {
 
-  var ariaSpecURL = {
-    "ED": "http://rawgithub.com/w3c/aria/master/spec/aria.html",
-    "WD" : "http://www.w3.org/TR/wai-aria-1.1/",
-    "REC": "http://www.w3.org/TR/wai-aria/"
-  }
+  var baseURL = respecConfig.ariaSpecURL[respecConfig.specStatus];
 
-  var baseURL = ariaSpecURL[respecConfig.specStatus];
-  
-  $ ('a.role-reference, a.property-reference, a.state-reference').each (
+  $ ('a.role-reference, a.property-reference, a.state-reference, a.specref').each (
     function (idx, el) {
       var href = $ (el).attr ('href');
       $ (el).attr ('href', baseURL + href);
