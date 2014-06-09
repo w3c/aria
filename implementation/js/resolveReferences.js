07af3c331486216f06304194f4b5636b4d6a3e09
Shane McCarron <halindrome@gmail.com>
2014-06-09 10:30:27 -0500

    Added handler to update included terms file to resolve sref and pref elements as links into the main ARIA spec.

--- implementation/js/resolveReferences.js
+++ implementation/js/resolveReferences.js
@@ -10,3 +10,34 @@ function linkCrossReferences() {
   ); 
 }
 
+function updateReferences(base) {
+    // update references to properties
+
+    var baseURL = respecConfig.ariaSpecURLs[respecConfig.specStatus];
+
+    $.each(base.querySelectorAll("pref, sref, rref"), function(i, item) {
+        var parentNode = item.parentNode;
+        var content = item.textContent || item.innerText;
+        var sp = document.createElement("a");
+        sp.className = (item.localName === "pref" ? "property-reference" : (item.localName === "sref" ? "state-reference" : "role-reference"));
+        var ref = item.getAttribute("title");
+        if (!ref) {
+            ref = content;
+        }
+        sp.href = baseURL + "#" + ref;
+        sp.setAttribute("title", content);
+        sp.innerHTML = content;
+        parentNode.replaceChild(sp, item);
+    });
+}
+
+// included files are brought in after proProc.  Create a DOM tree
+// of content then call the updateReferences method above on it.  Return
+// the transformed content
+function fixIncludes(utils, content) {
+    var base = document.createElement("div");
+    base.innerHTML = content;
+    updateReferences(base);
+    return (base.innerHTML);
+}
+
