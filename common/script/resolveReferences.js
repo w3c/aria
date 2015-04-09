3cb3055edecd8a30278625af94fe962a55be7a1d
Shane McCarron <halindrome@gmail.com>
2015-04-09 07:42:21 -0500

    Made changes as per @michael-n-cooper
    Moved handler script into common
    Also changed the master script aria.js to use the
    resolveReferences method in common.
    This required adding some defines to the master aria.html spec.

--- common/script/resolveReferences.js
+++ common/script/resolveReferences.js
@@ -67,13 +67,47 @@ function updateReferences(base) {
     });
 }
 
-// included files are brought in after proProc.  Create a DOM tree
-// of content then call the updateReferences method above on it.  Return
-// the transformed content
-function fixIncludes(utils, content) {
+// We should be able to remove terms that are not actually
+// referenced from the common definitions
+var termNames = [] ;
+
+function restrictReferences(utils, content) {
     var base = document.createElement("div");
     base.innerHTML = content;
     updateReferences(base);
+
+    // strategy: Traverse the content finding all of the terms defined
+    $.each(base.querySelectorAll("dfn"), function(i, item) {
+        var $t = $(item) ;
+        var title = $t.dfnTitle();
+        var n = $t.makeID("dfn", title);
+        if (n) {
+            termNames[n] = $t.parent() ;
+        }
+    });
+
+    // add a handler to come in after all the definitions are resolved
+
+    respecEvents.sub('end', function(message) {
+        if (message == 'core/link-to-dfn') {
+            // all definitions are linked
+            $("a.internalDFN").each(function () {
+                var $item = $(this) ;
+                var r = $item.attr('href').replace(/^#/,"") ;
+                if (termNames[r]) {
+                    delete termNames[r] ;
+                }
+            });
+    // delete any terms that were not referenced.
+            Object.keys(termNames).forEach(function(term) {
+                var $p = $("#"+term) ;
+                if ($p) {
+                    $p.next().remove();
+                    $p.remove() ;
+                }
+            });
+        }
+    });
     return (base.innerHTML);
 }
 
