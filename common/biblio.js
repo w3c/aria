2e6a85aff3622a1a1ee139197bb4a10eb1467ecf
Michael Cooper <cooper@w3.org>
2015-07-10 15:43:47 -0400

    HTML AAM bibref fix

--- common/biblio.js
+++ common/biblio.js
@@ -81,12 +81,13 @@ var biblio = {
 			"Alexander Surkov"
 		],
 		"etAl": true,
-		"href": "http://www.w3.org/TR/html-aam/",
-		"title": "HTML Accessibility API Mappings 1.1",
-		"status": "ED",
+		"href": "http://www.w3.org/TR/html-aam-1.0/",
+		"title": "HTML Accessibility API Mappings 1.0",
+		"status": "WD",
 		"publisher": "W3C",
 		"deliveredBy": [
-			"http://www.w3.org/html"
+			"http://www.w3.org/html",
+			"http://www.w3.org/WAI/PF/"
 		]
 	},
 	"MSAA": {
