23824fae887197a1ca226215379924e3e3d63254
Joanmarie Diggs <jdiggs@igalia.com>
2018-01-19 17:54:55 -0500

    Modify the mapping-maintainers utility to work with and without Respec

--- core-aam/common/script/utility.js
+++ core-aam/common/script/utility.js
@@ -1,15 +1,24 @@
 /* Utility functions */
 
+//check for require() and respec context
+if (typeof require !== "undefined") {
+    require(["core/pubsubhub"], function(respecEvents) {
+        respecEvents.sub('end', function(message) {
+            if (message === 'core/link-to-dfn')
+                convertAuthorsToMaintainers();
+        });
+    });
+} else {
+    $(document).ready(function() {
+	convertAuthorsToMaintainers();
+    });
+}
+
 // Change the authors credit to mapping contributors credit
 // Right now this will run on any author credit, not scoped to AAMs
-require(["core/pubsubhub"], function(respecEvents) {
+function convertAuthorsToMaintainers() {
     "use strict";
-    respecEvents.sub('end', function(message) {
-    	if (message === 'core/link-to-dfn') {
-    		document.querySelectorAll("div.head dt").forEach(function(node){
-    			if (node.textContent == "Authors:") node.textContent = "Platform Mapping Maintainers:";
-    		});
-    	}
-	})
-})
-
+    document.querySelectorAll("div.head dt").forEach(function(node){
+        if (node.textContent == "Authors:") node.textContent = "Platform Mapping Maintainers:";
+    });
+}
