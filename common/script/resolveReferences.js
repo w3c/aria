60b1b9cb1fae1156e7a77228a8482f13ec4e3386
michael-n-cooper <cooper@w3.org>
2018-04-13 22:48:49 +0000

    TRAVIS-CI update from aria-common

--- common/script/resolveReferences.js
+++ common/script/resolveReferences.js
@@ -247,12 +247,13 @@ require(["core/pubsubhub"], function(respecEvents) {
             });
     // delete any terms that were not referenced.
             Object.keys(termNames).forEach(function(term) {
-                var $p = $("#"+term) ;
+                var $p = $("#"+term);
                 if ($p) {
-                    var tList = $p.getDfnTitles();
+                    // Delete altered dfn elements and refs
                     $p.parent().next().remove();
-                    $p.remove() ;
-                    tList.forEach(function( item ) {
+                    $p.parent().remove();
+
+                    $p.getDfnTitles().forEach(function( item ) {
                         if (respecConfig.definitionMap[item]) {
                             delete respecConfig.definitionMap[item];
                         }
