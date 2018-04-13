4390d40f89a17a20c3bb280155b71095a1ccd07d
michael-n-cooper <cooper@w3.org>
2018-04-13 22:44:07 +0000

    TRAVIS-CI update from aria-common

--- core-aam/common/script/utility.js
+++ core-aam/common/script/utility.js
@@ -1,24 +1,15 @@
 /* Utility functions */
 
-//check for require() and respec context
-if (typeof require !== "undefined") {
-    require(["core/pubsubhub"], function(respecEvents) {
-        respecEvents.sub('end', function(message) {
-            if (message === 'core/link-to-dfn')
-                convertAuthorsToMaintainers();
-        });
-    });
-} else {
-    $(document).ready(function() {
-	convertAuthorsToMaintainers();
-    });
-}
-
 // Change the authors credit to mapping contributors credit
 // Right now this will run on any author credit, not scoped to AAMs
-function convertAuthorsToMaintainers() {
+require(["core/pubsubhub"], function(respecEvents) {
     "use strict";
-    document.querySelectorAll("div.head dt").forEach(function(node){
-        if (node.textContent == "Authors:") node.textContent = "Platform Mapping Maintainers:";
-    });
-}
+    respecEvents.sub('end', function(message) {
+    	if (message === 'core/link-to-dfn') {
+    		document.querySelectorAll("div.head dt").forEach(function(node){
+    			if (node.textContent == "Authors:") node.textContent = "Platform Mapping Maintainers:";
+    		});
+    	}
+	})
+})
+
