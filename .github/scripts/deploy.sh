0ec56215e7880effc15abe02f02d061f05be536a
plehegar <plh@w3.org>
2021-11-08 20:10:00 -0500

    Another approach

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,22 +1,73 @@
 #!/bin/bash
+set -exu
+# e: Exit immediately if a command exits with a non-zero status
+# x: Print commands and their arguments as they are executed
+# u: Treat unset variables as an error when substituting
 
-if [ $FOLDER = "" ]
-then
- echo "build-output needs a build directory name in FOLDER env variable"
- exit 1
-fi
+# FOLDER: env variable in your YML file to indicate the output directory
+# GITHUB_TOKEN: env variable for ${{ secrets.GITHUB_TOKEN }} in your YML file
 
-TMPDIR=$FOLDER
+echo "FOLDER: ${FOLDER}"
 
+# set the GitHub credentials and env
+git config --global user.email w3cbot-gh@w3.org
+git config --global user.name w3cbot
+git config --global user.password $GITHUB_TOKEN
 
-echo Cleaning $TMPDIR before building
+REPO_URL="https://w3cbot:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
-rm -rf ${TMPDIR}
-mkdir ${TMPDIR}
+echo Cleaning $FOLDER before building
+
+MAIN=$PWD
+
+rm -rf $FOLDER
+
+echo Using $GITHUB_REPOSITORY
+
+git clone $REPO_URL $FOLDER
+
+cd $FOLDER
+
+TARGET=$PWD
+TARGET_BRANCH=gh-pages-2
+
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+
+cd $MAIN
 
 # Place your build operations below
+# new files should go into $TARGET
 
-echo Now building the output in the director ./$TMPDIR
+echo Now building the output in $TARGET
 
-cp -R common img validator-tests ${TMPDIR}
-curl https://labs.w3.org/spec-generator/?type=respec"&"url=https://raw.githack.com/w3c/aria/main/index.html -o ${TMPDIR}/index.html -f  --retry 3
+cp -R common img validator-tests ${TARGET}
+curl https://labs.w3.org/spec-generator/?type=respec"&"url=https://raw.githack.com/w3c/aria/main/index.html -o ${TARGET}/index.html -f  --retry 3
+
+# End of building the
+
+cd ${TARGET}
+
+if [ $TRAVIS_PULL_REQUEST != "false" ]
+then
+  # this is a pull request so exit
+  exit 0
+fi
+
+if [[ -z $(git status --porcelain) ]]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+echo Add the changes
+
+git add -A .
+
+echo Commit the changes
+
+git commit -m ":robot: Deploy to GitHub Pages: $GITHUB_SHA from branch \"$TRAVIS_BRANCH\""
+
+echo Attempt to push
+
+git push $REPO_URL $TARGET_BRANCH
+
+echo done
