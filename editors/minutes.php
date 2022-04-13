6924bdf378a012f28d58ece2056f8d328ce651db
Michael Cooper <cooper@w3.org>
2022-04-13 09:42:22 -0400

    minutes for editors

--- editors/minutes.php
+++ editors/minutes.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
 	<head>
-<?php include "_head.phi"; ?>
-		<title>ARIA Minutes</title>
+<?php include "../_head.phi"; ?>
+		<title>ARIA Editors Minutes</title>
 	</head>
 	<body>
-<?php include "_header.phi"; ?>
-		<h1>ARIA Minutes</h1>
+<?php include "../_header.phi"; ?>
+		<h1>ARIA Editors Minutes</h1>
 		<?php
-	$channel_id = array("aria", "aria-dive");
-	include "../../2022/01/minutes/default-view.phi";
+	$channel_id = array("aria-editors");
+	include "../../../2022/01/minutes/default-view.phi";
 	?>
-<?php include "_footer.phi"; ?>
+<?php include "../_footer.phi"; ?>
 	</body>
 </html>
